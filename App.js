0e0545740a3f1f1db2d38f5a140f004ab455824a
RoyC1657 <corellaroyner165782@gmail.com>
2025-12-10 22:14:01 -0500

    Added Folder structure and Initial Hello World SFcreen as a place holder

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style = {{fontSize: 34, color: "red", fontWeight: "bold" }}>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import HelloWorldScreen from './src/screens/InitialScreen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return <HelloWOrldScreen />;
+}
---
+++ src/screens/HelloWorldScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function HelloWorldScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
