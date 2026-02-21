d5e09474c5ad0dd7f63f158c043972de0608a277
RoyC1657 <corellaroyner165782@gmail.com>
2026-02-21 04:44:21 -0500

    fixed db.js for postgresql

--- server/db.js
+++ server/db.js
@@ -5,31 +5,33 @@ const pool = new Pool({
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 })
 
-const db = new Database ('tipout.db')
+async function initDB() {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS employees (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL,
+      roles TEXT NOT NULL
+    );
 
-db.exec(`
-  CREATE TABLE IF NOT EXISTS employees (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    roles TEXT NOT NULL
-  );
+    CREATE TABLE IF NOT EXISTS shifts (
+      id SERIAL PRIMARY KEY,
+      date TEXT NOT NULL,
+      total_cash REAL NOT NULL,
+      total_credit REAL NOT NULL
+    );
 
-  CREATE TABLE IF NOT EXISTS shifts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date TEXT NOT NULL,
-    total_cash REAL NOT NULL,
-    total_credit REAL NOT NULL
-  );
+    CREATE TABLE IF NOT EXISTS shift_employees (
+      id SERIAL PRIMARY KEY,
+      shift_id INTEGER NOT NULL,
+      employee_id INTEGER NOT NULL,
+      hours_worked REAL NOT NULL,
+      role_worked TEXT NOT NULL,
+      FOREIGN KEY (shift_id) REFERENCES shifts(id),
+      FOREIGN KEY (employee_id) REFERENCES employees(id)
+    );
+  `)
+}
 
-  CREATE TABLE IF NOT EXISTS shift_employees (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    shift_id INTEGER NOT NULL,
-    employee_id INTEGER NOT NULL,
-    hours_worked REAL NOT NULL,
-    role_worked TEXT NOT NULL,
-    FOREIGN KEY (shift_id) REFERENCES shifts(id),
-    FOREIGN KEY (employee_id) REFERENCES employees(id)
-  );
-`)
+initDB()
 
-module.exports = db
+module.exports = pool
